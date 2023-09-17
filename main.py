e199fd941389323f60c66ab5d07445de386c35c0
Evgeniy <119398191+evgenii-erokhin@users.noreply.github.com>
2023-09-17 22:55:38 +0300

    Created two drafts of func for current and forecast wearher

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import logging
 import os
+from pprint import pprint
+
 import requests
 from dotenv import load_dotenv
-
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 load_dotenv()
 
@@ -14,15 +18,43 @@ CURRENT_ENDPOINT = 'https://api.gismeteo.net/v2/weather/current/?'
 HEADERS = {'X-Gismeteo-Token': GISMETIO_TOKEN}
 
 
-def get_current_weather(coordinates):
-    pass
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=('Добро пожаловать! Я бот прогноза погоды. Выбери в Меню'
+              ' интересующий тип прогноза и я пришлю его тебе')
+    )
 
 
-def get_weather_forecast():
-    pass
+async def get_current_weather(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    payload = {
+        'latitude': 57.7,
+        'longitude': 40.94,
+    }
+    r = requests.get(CURRENT_ENDPOINT, headers=HEADERS, params=payload)
+    pressure = r.json().get('response').get('pressure').get('mm_hg_atm')
+    temperature = r.json().get('response').get('temperature').get('air').get('C')
+    discribe = r.json().get('response').get('description').get('full')
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'Прогноз погоды на сейчас: давление - {pressure} мм. рт. ст.'
+        f'Температура воздуха {temperature} градусов C.'
+        f' {discribe}'
+        )
 
 
-def get_my_coordinates():
+async def get_weather_forecast(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    payload = {
+        'latitude': 57.7,
+        'longitude': 40.94,
+        'days': 1
+    }
+    r = requests.get(FORCAST_ENDPOINT, headers=HEADERS, params=payload)
+    pprint(r.json().get('response'))
+
+
+def get_my_coordinates(update, context):
     pass
 
 
@@ -31,7 +63,21 @@ def send_message():
 
 
 def main():
-    pass
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='main.log',
+        filemode='w',
+        format='%(asctime)s - %(levelname)s - %(message)s - %(name)s'
+    )
+    application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    starting = CommandHandler('start', start)
+    current_weather = CommandHandler('current_weather', get_current_weather)
+    forecast_weather = CommandHandler('forecast_weather', get_weather_forecast)
+    application.add_handler(starting)
+    application.add_handler(current_weather)
+    application.add_handler(forecast_weather)
+
+    application.run_polling()
 
 
 if __name__ == '__main__':
